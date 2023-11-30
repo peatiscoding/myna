2d94d929ce0afe854fa0e290ebb2e6c942a42cf3
Kittiphat S <freeuxer@gmail.com>
2023-11-30 14:17:24 +0700

    added ollama, added in-memory to load the data.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
 export const config = {
 	openAI: {
 		key: process.env.OPENAI_KEY,
-	}
+	},
+	ollama: {
+		baseUrl: 'http://localhost:11434',
+		defaultModel: 'mistral',
+	},
 }
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,61 @@
-import { OpenAI } from 'langchain/llms/openai'
-import { ChatOpenAI } from 'langchain/chat_models/openai'
-import { HumanMessage } from 'langchain/schema'
+import { RunnableSequence } from 'langchain/schema/runnable'
+import { ChatPromptTemplate, MessagesPlaceholder } from 'langchain/prompts'
+import { BufferMemory } from 'langchain/memory'
+
+import { ChatOllama } from 'langchain/chat_models/ollama'
 
 // ESM: relative need .js suffix
 import { config } from './config.js'
 
-console.log('initializing with', config.openAI.key)
+console.log('initializing with', config.ollama.baseUrl)
 
 // Engines
-const llm = new OpenAI({
-  openAIApiKey: config.openAI.key,
-  temperature: 0.9,
-})
-const chatModel = new ChatOpenAI({
-  openAIApiKey: config.openAI.key,
+const llm = new ChatOllama({
+  baseUrl: config.ollama.baseUrl,
+  model: config.ollama.defaultModel,
 })
 
 // Intercept inputs
-const text = 'what would be a good name for the CDP project?'
-const messages = [new HumanMessage({ content: text })]
-console.info('Prompt >', text)
-
-// Predict results
-const [llmResult, chatModelResult] = await Promise.all([
-  llm.predictMessages(messages),
-  chatModel.predictMessages(messages),
+const prompt = ChatPromptTemplate.fromMessages([
+  ['system', 'You are a helpful chatbot'],
+  new MessagesPlaceholder('history'),
+  ['human', '{input}'],
 ])
 
+const memory = new BufferMemory({
+  returnMessages: true,
+  inputKey: 'input',
+  outputKey: 'output',
+  memoryKey: 'history',
+})
+
+const chain = RunnableSequence.from([
+  {
+    input: (initialInput) => initialInput.input,
+    memory: () => memory.loadMemoryVariables({}),
+  },
+  {
+    input: (previousOutput) => previousOutput.input,
+    history: (previousOutput) => previousOutput.memory.history,
+  },
+  prompt,
+  llm,
+])
+
+// Predict results
+const inputs = {
+  input: 'Hello I am Peat',
+}
+const response = await chain.invoke(inputs)
+
+console.log('Respons', response.content)
 // Output
-console.log('llmResult', llmResult)
-console.log('chatModelResult', chatModelResult)
+await memory.saveContext(inputs, {
+  output: response.content,
+})
+
+const inputs2 = {
+  input: 'What is my name?',
+}
+const response2 = await chain.invoke(inputs2)
+console.log('Response', response2.content)
